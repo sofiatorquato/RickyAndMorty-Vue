0ae6aac1278ec9e30a315adc2b4697988516d7bc
Sofia <so2003barboza@gmail.com>
2026-05-18 15:01:30 -0300

    finalização com estilizações e responsividade

--- src/shared/components/Modal.vue
+++ src/shared/components/Modal.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { PersonagemCompleto } from '../types/paramsPersonas';
-import { ref, computed } from 'vue';
+import { computed } from 'vue';
 
 const props = defineProps<{
   exibir: boolean;
